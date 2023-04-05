0f58d8318081a12c72b1c08f3a9c526e055f1c39
Gleb Boiko <glebboyko@icloud.com>
2023-04-05 22:22:19 +0300

    Modified number.hpp

--- number.hpp
+++ number.hpp
@@ -1,24 +1,33 @@
 #pragma once
 #include "for_society.hpp"
 
+namespace number {
+
+template <typename A, typename B, typename C>
+struct Trio {
+  A first;
+  B second;
+  C third;
+};
+
 class Number {
  public:
-  Number() = default;
-  Number(uint8_t number, uint8_t mantissa, bool sign) noexcept;
+  Number() noexcept;
   Number(const Number& other) noexcept;
-
   Number& operator=(const Number&) noexcept;
 
   std::string GetNumber() const noexcept;
 
   void SignButton() noexcept;
-  void MantissaButton();
-  void NumberButton(uint8_t);
-  void ClearButton();
+  void MantissaButton();  // не уверен, может ли мантисса выдать ошибку
+  void NumberButton(uint8_t) noexcept;
+  void ClearButton() noexcept;
+
+  // for backup / restore
+  Number(uint8_t number, uint8_t mantissa, bool sign) noexcept;
+  Trio<uint8_t, uint8_t, bool> GetClass() const noexcept;
 
  private:
-  uint8_t number_ = 0;
-  uint8_t mantissa_ = UINT8_MAX;
-  bool sign_ = false;
-
 };
+
+}  // namespace number
