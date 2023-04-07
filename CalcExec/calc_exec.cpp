35ea5f4b76b4ea0eb72c4a726cab607c10840dfe
Gleb Boiko <glebboyko@icloud.com>
2023-04-07 21:15:45 +0300

    Реализованы некоторые методы

--- CalcExec/calc_exec.cpp
+++ CalcExec/calc_exec.cpp
@@ -1,1 +1,19 @@
 #include "calc_exec.hpp"
+
+using namespace CE;
+
+const std::vector<OperationCodes>& Calc::GetProgramBuffer() const noexcept {
+  return program_;
+}
+
+const CM::Buffer& Calc::GetRegisterBuffer() const noexcept { return buffer_; }
+
+Button Calc::GetCurrFuncButton() const noexcept { return curr_func_button_; }
+
+uint8_t Calc::GetStep() const noexcept { return FS::ToHexadecimal(step_); }
+
+Mode Calc::GetMode() const noexcept { return mode_; }
+
+void Calc::PressButton(Button button) {
+
+}
