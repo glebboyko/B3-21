6ca29a4b6ea73879f9f4abab1c609dc567aeabb3
Gleb Boiko <glebboyko@icloud.com>
2023-04-13 03:44:00 +0300

    Добавлена реализация некоторых функций/методов

--- CalcExec/calc_exec.cpp
+++ CalcExec/calc_exec.cpp
@@ -2,18 +2,89 @@
 
 using namespace CE;
 
-const std::vector<OperationCodes>& Calc::GetProgramBuffer() const noexcept {
-  return program_;
+bool IsNum(Button button) noexcept { return GetDigit(button, 2) == 4; }
+
+uint8_t GetDigit(uint8_t number, uint8_t index) noexcept {
+  for (uint8_t i = 0; i < index; ++i) {
+    number /= 10;
+  }
+  return number % 10;
 }
+
+const CP::Program& Calc::GetProgram() const noexcept { return program_; }
 
 const CM::Buffer& Calc::GetRegisterBuffer() const noexcept { return buffer_; }
 
 Button Calc::GetCurrFuncButton() const noexcept { return curr_func_button_; }
 
-uint8_t Calc::GetStep() const noexcept { return FS::ToHexadecimal(step_); }
-
 Mode Calc::GetMode() const noexcept { return mode_; }
 
 void Calc::PressButton(Button button) {
-
+  if (button == ButP || button == ButF) {  // change curr task
+    PressedFuncButton(button);
+  } else if (curr_func_button_ == ButP && button == ButStepRight) {
+    ChangeMode(Working);
+  } else if (curr_func_button_ == ButP && button == ButStepLeft) {
+    ChangeMode(Programming);
+  } else if (mode_ == Working) {
+    PressedButtonWorking(button);
+  } else if (mode_ == Programming) {
+    PressedButtonProgramming(button);
+  }
 }
+
+void Calc::TurnOn() noexcept { mode_ = Working; }
+
+void Calc::TurnOff() noexcept { *this = Calc(); }
+
+void Calc::ChangeMode(Mode new_mode) noexcept {
+  curr_func_button_ = ButNull;  // ?
+  mode_ = new_mode;
+}
+
+CP::OperationCodes Calc::GetOperationCode(CE::Button button) const noexcept {
+  return static_cast<CP::OperationCodes>((button / 1000) * 10 +
+                                         GetDigit(button, curr_func_button_));
+}
+
+void Calc::PressedButtonWorking(Button button) {
+  if (curr_func_button_ != ButNull && IsNum(button)) {
+    curr_func_button_ == ButP ? PNum(button) : FNum(button);
+  } else if (IsNum(button)) {
+    Num(button);
+  } else if (button == ButStepRight) {
+    program_.ExecuteStep(buffer_);
+  } else if (button == ButStepLeft) {
+    program_.MakeStep(CP::DirLeft);
+  } else {
+    (this->*method_ptr_[GetOperationCode(button)])();
+  }
+}
+
+void Calc::PressedButtonProgramming(Button button) {
+  if (button == ButStepRight) {
+    program_.MakeStep(CP::DirRight);
+  } else if (button == ButStepLeft) {
+    program_.MakeStep(CP::DirLeft);
+  } else {
+    program_.EnterCode(GetOperationCode(button));
+    curr_func_button_ = ButNull;
+  }
+}
+
+void Calc::PressedFuncButton(Button button) noexcept {
+  curr_func_button_ = button;
+}
+
+void Calc::PNum(uint8_t num) { buffer_.PutFronXToZ(num); }
+
+void Calc::FNum(uint8_t num) { buffer_.PutFromZToX(num); }
+
+void Calc::Num(uint8_t digit) { buffer_.GetX0().NumberButton(digit); }
+
+Calc::Calc(const CP::Program& program_buffer, const CM::Buffer& register_buffer,
+           CE::Button curr_func_button, CE::Mode mode)
+    : program_(program_buffer),
+      buffer_(register_buffer),
+      curr_func_button_(curr_func_button),
+      mode_(mode) {}
