4a48df35b8407e6a9fe06bb13837e23305867aa2
Gleb Boiko <glebboyko@icloud.com>
2023-04-17 23:54:41 +0300

    Изменен main

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
-#include <iostream>
-#include "CalcExec/calc_exec.hpp"
+#include <wx/wx.h>
 
-int main() {}
+#include "backend/calc_exec/calc_exec.hpp"
+#include "backend/program_buffer_exec/program.hpp"
+
+int main() {  // Она здесь, чтобы проект собирался
+}
