a15c13b3f98eb87bb1fa79acb4b706a2b7ee2b73
Gleb Boiko <glebboyko@icloud.com>
2023-04-19 22:10:50 +0300

    Изменено название функции

--- backend/for_society/for_society.cpp
+++ backend/for_society/for_society.cpp
@@ -7,18 +7,18 @@ uint8_t GetDigit(uint8_t number, uint8_t index) noexcept {
   return number % 10;
 }
 
-template <uint64_t from_sys, uint64_t to_sys>
-uint64_t FromSysToSys(uint64_t number) {
+template <uint64_t from_not, uint64_t to_not>
+uint64_t FromNotToNot(uint64_t number) noexcept {
   uint64_t result = 0;
 
-  for (uint64_t multiply = 1; number != 0; number /= 10, multiply *= from_sys) {
+  for (uint64_t multiply = 1; number != 0; number /= 10, multiply *= from_not) {
     result += multiply * (number % 10);
   }
 
   number = 1;
-  for (; result != 0; result /= to_sys) {
+  for (; result != 0; result /= to_not) {
     number *= 10;
-    number += result % to_sys;
+    number += result % to_not;
   }
   for (; number != 1; number /= 10) {
     result *= 10;
--- backend/for_society/for_society.hpp
+++ backend/for_society/for_society.hpp
@@ -3,8 +3,8 @@
 
 namespace FS {
 
-template <uint64_t, uint64_t>
-uint64_t FromSysToSys(uint64_t);
+template <uint64_t from_not, uint64_t to_not>
+uint64_t FromNotToNot(uint64_t) noexcept;
 
 // extract digit from integer by index
 uint8_t GetDigit(uint8_t number, uint8_t index) noexcept;
