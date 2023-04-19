03b74edd4b475d2199f01446500e39bb81920c24
Gleb Boiko <glebboyko@icloud.com>
2023-04-19 16:49:16 +0300

    Убраны friend

--- backend/number/number.hpp
+++ backend/number/number.hpp
@@ -32,34 +32,22 @@ class Number {
   Number(uint8_t characteristic, uint8_t mantissa, bool sign) noexcept;
   Trio<uint8_t, uint8_t, bool> GetClass() const noexcept;
 
-  friend bool operator<(const Number&, const Number&);
-  friend bool operator<=(const Number&, const Number&);
-  friend bool operator>(const Number&, const Number&);
-  friend bool operator>=(const Number&, const Number&);
-  friend bool operator==(const Number&, const Number&);
-  friend bool operator!=(const Number&, const Number&);
+  bool operator<(const Number&);
+  bool operator<=(const Number&);
+  bool operator>(const Number&);
+  bool operator>=(const Number&);
+  bool operator==(const Number&);
+  bool operator!=(const Number&);
 
-  friend Number operator+(const Number&, const Number&);
-  friend Number operator-(const Number&, const Number&);
-  friend Number operator*(const Number&, const Number&);
-  friend Number operator/(const Number&, const Number&);
+  Number operator+(const Number&);
+  Number operator-(const Number&);
+  Number operator*(const Number&);
+  Number operator/(const Number&);
 
  private:
   uint8_t number_ = 0;
   uint8_t mantissa_ = UINT8_MAX;
   bool sign_ = false;
 };
-
-bool operator<(const Number&, const Number&);
-bool operator<=(const Number&, const Number&);
-bool operator>(const Number&, const Number&);
-bool operator>=(const Number&, const Number&);
-bool operator==(const Number&, const Number&);
-bool operator!=(const Number&, const Number&);
-
-Number operator+(const Number&, const Number&);
-Number operator-(const Number&, const Number&);
-Number operator*(const Number&, const Number&);
-Number operator/(const Number&, const Number&);
 
 }  // namespace CN
