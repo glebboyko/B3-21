277ee0bf3425b7b6505ae6e817c3bfd0bb617baf
TheBetelgeuse <the.cyber.neo@gmail.com>
2023-04-23 22:19:30 +0300

    Добавлена поддержка элементарных операций, определенных в описании проекта. Также реализован собственный класс функций Math, который позволяет с указанной точностью считать математические функции для CN::Number

---
+++ backend/calc_exec/calc_math.hpp
@@ -0,0 +1,114 @@
+#include "backend/Number/Number.hpp"
+
+namespace Math {
+
+  const CN::Number kPi = static_cast<float>(3.14159265359);
+
+  CN::Number Pow(CN::Number num, CN::Number power) {
+    auto res = num;
+    for (CN::Number i = 0; i < power; i = i + CN::Number(1)) {
+      res *= res;
+    }
+    return res;
+  }
+
+  CN::Number Factorial(CN::Number n) {
+      if (n == 0 || n == 1) {
+          return CN::Number(1);
+      }
+      CN::Number result = 1;
+      for (CN::Number i = 2; i <= n; i = i + CN::Number(1)) {
+          result *= i;
+      }
+      return result;
+  }
+
+  CN::Number Exp(CN::Number x) {
+      CN::Number result = 1;
+      CN::Number term;
+      int i = 1;
+      CN::Number epsilon = static_cast<float>(1e-6);
+
+      do {
+          term = Pow(x, i) / Factorial(i);
+          result += term;
+          ++i;
+      } while (term > epsilon);
+
+      return result;
+  }
+
+  CN::Number Sin(CN::Number x) {
+    CN::Number result = x;
+    CN::Number term;
+    int i = 3;
+      CN::Number epsilon = static_cast<float>(1e-6);
+    do {
+        term = (Pow(CN::Number(-1), i) * (Pow(x, i) / Factorial(i)));
+        result += term;
+        i += 2;
+    } while (term > epsilon);
+  }
+
+  CN::Number Cos(CN::Number x) {
+    CN::Number result = 1;
+    CN::Number term;
+    int i = 2;
+    CN::Number epsilon = static_cast<float>(1e-6);
+    do {
+        term = (Pow(CN::Number(-1), i + 1) * (Pow(x, i) / Factorial(i)));
+        result += term;
+        i += 2;
+    } while (term > epsilon);
+  }
+
+  CN::Number LnLimited(CN::Number x) {
+    CN::Number ln2 = static_cast<float>(0.693147181);
+    if (x != 1) {
+      CN::Number epsilon = static_cast<float>(1e-6);
+      CN::Number result = 0;
+      CN::Number prev_result;
+      CN::Number x_power = x;
+      int i = 1;
+
+      do {
+          prev_result = result;
+          CN::Number term = CN::Number(i % 2 == 1 ? 1 : -1) * Pow(x, i) / i;
+          result += term;
+          ++i;
+      } while (((result - prev_result) < 0 ? CN::Number(-1) * (result - prev_result) : result - prev_result) >= epsilon);
+
+      return result;
+      }
+    return ln2;
+  }
+
+  CN::Number Ln(CN::Number x) {
+    CN::Number result;
+    if (x <= 1 && x > -1) {
+      return LnLimited(x);
+    }
+    CN::Number y = x / 2;
+    int count = 1;
+    while (y >= 2) {
+      y = y / 2;
+      ++count;
+    }
+    return LnLimited(y - 1) + (CN::Number(count) * LnLimited(1));
+  }
+
+  CN::Number Root(CN::Number x) {
+      CN::Number epsilon = static_cast<float>(1e-6);
+      CN::Number y = x;
+      CN::Number y_next;
+      do {
+          y_next = y - (y * y - x) / (CN::Number(2) * y);
+          if (((y - y_next) < 0 ? CN::Number(-1) * (y - y_next) : (y - y_next)) < epsilon) {
+              break;
+          }
+          y = y_next;
+      } while (true);
+      return y_next;
+  }
+
+};
--- backend/calc_exec/calc_operations.cpp
+++ backend/calc_exec/calc_operations.cpp
@@ -1,62 +1,182 @@
 #include "calc_exec.hpp"
+#include "calc_math.hpp"
 
 namespace CE {
 
-void Calc::CP() {
-  // подготовка
-  if (exec_prog_thread_.has_value()) {
-    exec_prog_thread_.value().join();
-  }
-  ChangeMode(ExecutingProg);
-
-  // распараллеливание
-  exec_prog_thread_ = std::thread([this]() {
-    // исполняем, пока можно
-    while (mode_ == ExecutingProg) {
-      // здоровый сон
-      std::this_thread::sleep_for(kWait);
-
-      // выполняем команду
-      CP::ProgramStatus executing_result = program_->ExecuteStep(*this);
-
-      // проверяем результат выполнения
-      if (executing_result == CP::Stop) {
-        break;
-      }
-      if (executing_result == CP::Error) {
-        SendSignal(Error);
-        break;
-      }
-
-      // обновляем визуализацию
-      SendSignal(UpdateData);
+  void Calc::CP() {
+    // подготовка
+    if (exec_prog_thread_.has_value()) {
+      exec_prog_thread_.value().join();
     }
+    ChangeMode(ExecutingProg);
 
-    ChangeMode(Working);
-  });
-}
+    // распараллеливание
+    exec_prog_thread_ = std::thread([this]() {
+      // исполняем, пока можно
+      while (mode_ == ExecutingProg) {
+        // здоровый сон
+        std::this_thread::sleep_for(kWait);
 
-void Calc::Neutral() {}
-void Calc::PArrowUp() {}
-void Calc::ArrowUp() {}
-void Calc::PXArrowY() {}
-void Calc::XArrowY() {}
-void Calc::PMultiply() {}
-void Calc::Multiply() {}
-void Calc::PDivision() {}
-void Calc::Division() {}
-void Calc::XExpY() {}
-void Calc::FComma() {}
-void Calc::Comma() {}
-void Calc::BO() {}
-void Calc::FPrefixMinus() {}
-void Calc::PrefixMinus() {}
-void Calc::FVP() {}
-void Calc::VP() {}
-void Calc::Cx() {}
-void Calc::PMinus() {}
-void Calc::Minus() {}
-void Calc::PPlus() {}
-void Calc::Plus() {}
+        // выполняем команду
+        CP::ProgramStatus executing_result = program_->ExecuteStep(*this);
 
-}
+        // проверяем результат выполнения
+        if (executing_result == CP::Stop) {
+          break;
+        }
+        if (executing_result == CP::Error) {
+          SendSignal(Error);
+          break;
+        }
+
+        // обновляем визуализацию
+        SendSignal(UpdateData);
+      }
+
+      ChangeMode(Working);
+    });
+  }
+
+  void Calc::Neutral() {
+    curr_func_button_ = ButNull;
+    SendSignal(UpdateData);
+  }
+
+  void Calc::PArrowUp() {
+    CN::Number& x_0 = buffer_.GetX0();
+    x_0 = Math::Exp(x_0);
+    SendSignal(UpdateData);
+    curr_func_button_ = ButNull;
+
+  }
+
+  void Calc::ArrowUp() {
+    buffer_.PutFromXToZ(1);
+    SendSignal(UpdateData);
+    curr_func_button_ = ButNull;
+  }
+
+  void Calc::PXArrowY() {
+    CN::Number& x_0 = buffer_.GetX0();
+    x_0 = Math::Ln(x_0);
+    SendSignal(UpdateData);
+    curr_func_button_ = ButNull;
+  }
+
+  void Calc::XArrowY() {
+    CN::Number tmp = buffer_.GetNumeratedBuffer()[1];
+    buffer_.PutFromZToX(1);
+    buffer_.GetX0() = tmp;
+    SendSignal(UpdateData);
+    curr_func_button_ = ButNull;
+  }
+
+  void Calc::PMultiply() {
+    CN::Number& x_0 = buffer_.GetX0();
+    x_0 = Math::kPi;
+    SendSignal(UpdateData);
+    curr_func_button_ = ButNull;
+  }
+
+  void Calc::Multiply() {
+    CN::Number y = buffer_.GetNumeratedBuffer()[1];
+    CN::Number& x = buffer_.GetX0();
+    x = x * y;
+    SendSignal(UpdateData);
+    curr_func_button_ = ButNull;
+  }
+
+  void Calc::PDivision() {
+    PArrowUp();
+  }
+
+  void Calc::Division() {
+    CN::Number y = buffer_.GetNumeratedBuffer()[1];
+    CN::Number& x = buffer_.GetX0();
+    x = x / y;
+    SendSignal(UpdateData);
+    curr_func_button_ = ButNull;
+  }
+
+  void Calc::XExpY() {
+    CN::Number y = buffer_.GetNumeratedBuffer()[1];
+    CN::Number& x = buffer_.GetX0();
+    x = Math::Pow(x, y);
+    SendSignal(UpdateData);
+    curr_func_button_ = ButNull;
+  }
+
+  void Calc::FComma() {
+    CN::Number& x = buffer_.GetX0();
+    x = CN::Number(1) / x;
+    SendSignal(UpdateData);
+    curr_func_button_ = ButNull;
+  }
+
+  void Calc::Comma() {
+    SendSignal(UpdateData);
+    curr_func_button_ = ButNull;
+  }
+
+  void Calc::BO() {
+    curr_func_button_ = ButNull;
+  }
+  void Calc::FPrefixMinus() {
+    CN::Number& x = buffer_.GetX0();
+    x = Math::Pow(x, 2);
+    SendSignal(UpdateData);
+    curr_func_button_ = ButNull;
+  }
+
+  void Calc::PrefixMinus() {
+    CN::Number& x = buffer_.GetX0();
+    x = CN::Number(-1) * x;
+    SendSignal(UpdateData);
+    curr_func_button_ = ButNull;
+  }
+
+  void Calc::FVP() {
+    CN::Number& x = buffer_.GetX0();
+    x = Math::Root(x);
+    SendSignal(UpdateData);
+    curr_func_button_ = ButNull;
+  }
+
+  void Calc::VP() {
+
+  }
+  void Calc::Cx() {
+
+  }
+
+  void Calc::PMinus() {
+    CN::Number& x = buffer_.GetX0();
+    x = Math::Cos(x);
+    SendSignal(UpdateData);
+    curr_func_button_ = ButNull;
+  }
+
+  void Calc::Minus() {
+    CN::Number y = buffer_.GetNumeratedBuffer()[1];
+    CN::Number& x = buffer_.GetX0();
+    x = x - y;
+    SendSignal(UpdateData);
+    curr_func_button_ = ButNull;
+  }
+
+  void Calc::PPlus() {
+    CN::Number& x = buffer_.GetX0();
+    x = Math::Sin(x);
+    SendSignal(UpdateData);
+    curr_func_button_ = ButNull;
+  }
+
+  void Calc::Plus() {
+    CN::Number y = buffer_.GetNumeratedBuffer()[1];
+    CN::Number& x = buffer_.GetX0();
+    x = x - y;
+    SendSignal(UpdateData);
+    curr_func_button_ = ButNull;
+  }
+};
+
