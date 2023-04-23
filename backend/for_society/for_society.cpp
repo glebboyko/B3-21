58b42adb2b73dc04a0ac00068d7f6a45cbbf6c15
Gleb Boiko <glebboyko@icloud.com>
2023-04-23 20:32:25 +0300

    Исправлена ошибка

--- backend/for_society/for_society.cpp
+++ backend/for_society/for_society.cpp
@@ -9,25 +9,25 @@ uint32_t GetDigit(uint32_t number, uint32_t index) noexcept {
   return number % 10;
 }
 
-template <uint64_t from_not, uint64_t to_not>
-uint64_t FromNotToNot(uint64_t number) noexcept {
-  uint64_t result = 0;
-
-  for (uint64_t multiply = 1; number != 0; number /= 10, multiply *= from_not) {
-    result += multiply * (number % 10);
-  }
-
-  number = 1;
-  for (; result != 0; result /= to_not) {
-    number *= 10;
-    number += result % to_not;
-  }
-  for (; number != 1; number /= 10) {
-    result *= 10;
-    result += number % 10;
-  }
-
-  return result;
-}
+//template <uint64_t from_not, uint64_t to_not>
+//uint64_t FromNotToNot(uint64_t number) noexcept {
+//  uint64_t result = 0;
+//
+//  for (uint64_t multiply = 1; number != 0; number /= 10, multiply *= from_not) {
+//    result += multiply * (number % 10);
+//  }
+//
+//  number = 1;
+//  for (; result != 0; result /= to_not) {
+//    number *= 10;
+//    number += result % to_not;
+//  }
+//  for (; number != 1; number /= 10) {
+//    result *= 10;
+//    result += number % 10;
+//  }
+//
+//  return result;
+//}
 
 }
--- backend/for_society/for_society.hpp
+++ backend/for_society/for_society.hpp
@@ -4,7 +4,25 @@
 namespace FS {
 
 template <uint64_t from_not, uint64_t to_not>
-uint64_t FromNotToNot(uint64_t) noexcept;
+uint64_t FromNotToNot(uint64_t number) noexcept {
+  uint64_t result = 0;
+
+  for (uint64_t multiply = 1; number != 0; number /= 10, multiply *= from_not) {
+    result += multiply * (number % 10);
+  }
+
+  number = 1;
+  for (; result != 0; result /= to_not) {
+    number *= 10;
+    number += result % to_not;
+  }
+  for (; number != 1; number /= 10) {
+    result *= 10;
+    result += number % 10;
+  }
+
+  return result;
+}
 
 // extract digit from integer by index
 uint32_t GetDigit(uint32_t number, uint32_t index) noexcept;
