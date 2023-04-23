03c1c0153fc0ffd9e6aa2e95e3c5981afaf4ecb4
Gleb Boiko <glebboyko@icloud.com>
2023-04-23 18:47:32 +0300

    Исправлено взаимодействие с CP::Program

--- backend/calc_exec/calc_exec.hpp
+++ backend/calc_exec/calc_exec.hpp
@@ -17,53 +17,6 @@ class Program;
 
 namespace CE {
 
-const std::chrono::milliseconds kWait = std::chrono::milliseconds(250);
-
-const char kMessageQueueFile[] = "message_queue_file";
-
-enum MessageToVisualize {
-  UpdateData,
-  Error,
-  GoodBie
-};
-
-const uint32_t kOperationQuantity = 96;
-enum Mode { Working, Programming, ExecutingProg, TurnedOff };
-enum Button {
-  ButP = 0,
-  ButF = 1,
-  ButNull = 2,
-
-  ButNum0 = 421,
-  ButArrowUp = 653,
-  ButNum1 = 1421,
-  ButXArrowY = 1653,
-  ButNum2 = 2421,
-  ButMultiply = 2653,
-  ButNum3 = 3421,
-  ButDivision = 3653,
-  ButXExpY = 3879,
-  ButNum4 = 4421,
-  ButComma = 4653,
-  ButBO = 4879,
-  ButNum5 = 5421,
-  ButPrefixMinus = 5653,
-  ButBbP = 5879,
-  ButNum6 = 6421,
-  ButBvP = 6653,
-  ButPP = 6879,
-  ButNum7 = 7421,
-  ButCx = 7666,
-  ButCP = 7879,
-  ButNum8 = 8421,
-  ButMinus = 8653,
-  ButNum9 = 9421,
-  ButPlus = 9653,
-
-  ButStepLeft,
-  ButStepRight
-};
-
 class Calc {
  public:
   Calc();
@@ -87,7 +40,7 @@ class Calc {
        Button curr_func_button, Mode mode);
 
  private:
-  CP::Program program_;
+  CP::Program* program_;
   CM::Buffer buffer_ = CM::Buffer();
 
   Button curr_func_button_ = ButNull;
@@ -107,8 +60,10 @@ class Calc {
 
   void PressedFuncButton(Button);
 
+ public:
   void ExecuteCommand(CP::OperationCodes);
 
+ private:
   void SendSignal(MessageToVisualize);
 
   // проверочные методы
@@ -153,41 +108,103 @@ class Calc {
   typedef void (Calc::*MethodPtr)();
 
   MethodPtr method_ptr_[kOperationQuantity + 1] = {
-    /* 0  */ &Calc::Neutral,   &Calc::Neutral,      &Calc::Neutral,
-    /* 3  */ &Calc::PArrowUp,  &Calc::Neutral,      &Calc::Neutral,
-    /* 6  */ &Calc::ArrowUp,   &Calc::Neutral,      &Calc::Neutral,
-    /* 9  */ &Calc::Neutral,   &Calc::Neutral,      &Calc::Neutral,
-    /* 12 */ &Calc::Neutral,   &Calc::PXArrowY,     &Calc::Neutral,
-    /* 15 */ &Calc::Neutral,   &Calc::XArrowY,      &Calc::Neutral,
-    /* 18 */ &Calc::Neutral,   &Calc::Neutral,      &Calc::Neutral,
-    /* 21 */ &Calc::Neutral,   &Calc::Neutral,      &Calc::PMultiply,
-    /* 24 */ &Calc::Neutral,   &Calc::Neutral,      &Calc::Multiply,
-    /* 27 */ &Calc::Neutral,   &Calc::Neutral,      &Calc::Neutral,
-    /* 30 */ &Calc::Neutral,   &Calc::Neutral,      &Calc::Neutral,
-    /* 33 */ &Calc::PDivision, &Calc::Neutral,      &Calc::Neutral,
-    /* 36 */ &Calc::Division,  &Calc::Neutral,      &Calc::XExpY,
-    /* 39 */ &Calc::Neutral,   &Calc::Neutral,      &Calc::Neutral,
-    /* 42 */ &Calc::Neutral,   &Calc::Neutral,      &Calc::Neutral,
-    /* 45 */ &Calc::FComma,    &Calc::Comma,        &Calc::Neutral,
-    /* 48 */ &Calc::BO,        &Calc::Neutral,      &Calc::Neutral,
-    /* 51 */ &Calc::Neutral,   &Calc::Neutral,      &Calc::Neutral,
-    /* 54 */ &Calc::Neutral,   &Calc::FPrefixMinus, &Calc::PrefixMinus,
-    /* 57 */ &Calc::Neutral,   &Calc::Neutral,      &Calc::Neutral,
-    /* 60 */ &Calc::Neutral,   &Calc::Neutral,      &Calc::Neutral,
-    /* 63 */ &Calc::Neutral,   &Calc::Neutral,      &Calc::FVP,
-    /* 66 */ &Calc::VP,        &Calc::Neutral,      &Calc::Neutral,
-    /* 69 */ &Calc::Neutral,   &Calc::Neutral,      &Calc::Neutral,
-    /* 72 */ &Calc::Neutral,   &Calc::Neutral,      &Calc::Neutral,
-    /* 75 */ &Calc::Neutral,   &Calc::Cx,           &Calc::Neutral,
-    /* 78 */ &Calc::CP,        &Calc::Neutral,      &Calc::Neutral,
-    /* 81 */ &Calc::Neutral,   &Calc::Neutral,      &Calc::PMinus,
-    /* 84 */ &Calc::Neutral,   &Calc::Neutral,      &Calc::Minus,
-    /* 87 */ &Calc::Neutral,   &Calc::Neutral,      &Calc::Neutral,
-    /* 90 */ &Calc::Neutral,   &Calc::Neutral,      &Calc::Neutral,
-    /* 93 */ &Calc::PPlus,     &Calc::Neutral,      &Calc::Neutral,
-    /* 96 */ &Calc::Plus};
-
-  friend class CP::Program;
+      /* 0  */ &Calc::Neutral,
+      &Calc::Neutral,
+      &Calc::Neutral,
+      /* 3  */ &Calc::PArrowUp,
+      &Calc::Neutral,
+      &Calc::Neutral,
+      /* 6  */ &Calc::ArrowUp,
+      &Calc::Neutral,
+      &Calc::Neutral,
+      /* 9  */ &Calc::Neutral,
+      &Calc::Neutral,
+      &Calc::Neutral,
+      /* 12 */ &Calc::Neutral,
+      &Calc::PXArrowY,
+      &Calc::Neutral,
+      /* 15 */ &Calc::Neutral,
+      &Calc::XArrowY,
+      &Calc::Neutral,
+      /* 18 */ &Calc::Neutral,
+      &Calc::Neutral,
+      &Calc::Neutral,
+      /* 21 */ &Calc::Neutral,
+      &Calc::Neutral,
+      &Calc::PMultiply,
+      /* 24 */ &Calc::Neutral,
+      &Calc::Neutral,
+      &Calc::Multiply,
+      /* 27 */ &Calc::Neutral,
+      &Calc::Neutral,
+      &Calc::Neutral,
+      /* 30 */ &Calc::Neutral,
+      &Calc::Neutral,
+      &Calc::Neutral,
+      /* 33 */ &Calc::PDivision,
+      &Calc::Neutral,
+      &Calc::Neutral,
+      /* 36 */ &Calc::Division,
+      &Calc::Neutral,
+      &Calc::XExpY,
+      /* 39 */ &Calc::Neutral,
+      &Calc::Neutral,
+      &Calc::Neutral,
+      /* 42 */ &Calc::Neutral,
+      &Calc::Neutral,
+      &Calc::Neutral,
+      /* 45 */ &Calc::FComma,
+      &Calc::Comma,
+      &Calc::Neutral,
+      /* 48 */ &Calc::BO,
+      &Calc::Neutral,
+      &Calc::Neutral,
+      /* 51 */ &Calc::Neutral,
+      &Calc::Neutral,
+      &Calc::Neutral,
+      /* 54 */ &Calc::Neutral,
+      &Calc::FPrefixMinus,
+      &Calc::PrefixMinus,
+      /* 57 */ &Calc::Neutral,
+      &Calc::Neutral,
+      &Calc::Neutral,
+      /* 60 */ &Calc::Neutral,
+      &Calc::Neutral,
+      &Calc::Neutral,
+      /* 63 */ &Calc::Neutral,
+      &Calc::Neutral,
+      &Calc::FVP,
+      /* 66 */ &Calc::VP,
+      &Calc::Neutral,
+      &Calc::Neutral,
+      /* 69 */ &Calc::Neutral,
+      &Calc::Neutral,
+      &Calc::Neutral,
+      /* 72 */ &Calc::Neutral,
+      &Calc::Neutral,
+      &Calc::Neutral,
+      /* 75 */ &Calc::Neutral,
+      &Calc::Cx,
+      &Calc::Neutral,
+      /* 78 */ &Calc::CP,
+      &Calc::Neutral,
+      &Calc::Neutral,
+      /* 81 */ &Calc::Neutral,
+      &Calc::Neutral,
+      &Calc::PMinus,
+      /* 84 */ &Calc::Neutral,
+      &Calc::Neutral,
+      &Calc::Minus,
+      /* 87 */ &Calc::Neutral,
+      &Calc::Neutral,
+      &Calc::Neutral,
+      /* 90 */ &Calc::Neutral,
+      &Calc::Neutral,
+      &Calc::Neutral,
+      /* 93 */ &Calc::PPlus,
+      &Calc::Neutral,
+      &Calc::Neutral,
+      /* 96 */ &Calc::Plus};
 };
 
 uint32_t Calc::number_of_class_objects = 0;
