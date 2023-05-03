92b30c5973e94592ead5b7f2a1957f73c9573432
Gleb Boiko <glebboyko@icloud.com>
2023-05-03 21:43:50 +0300

    Перенесён класс, исправлена реализация метода

--- main.cpp
+++ main.cpp
@@ -1,3 +1,21 @@
+#include <memory>
+#include <thread>
+
 #include "frontend/interface/interface_def.hpp"
+#include "frontend/visualization/visualization.hpp"
+
+class CalculatorApp : public wxApp {
+ public:
+  virtual bool OnInit() {  // Создание главного окна калькулятора
+    std::shared_ptr<CE::Calc> calc(new CE::Calc());
+
+    ID::CalculatorFrame* frame = new ID::CalculatorFrame("Calculator", calc);
+    frame->Show(true);
+
+    std::thread updater = std::thread(IV::Updater, calc);
+    updater.detach();
+    return true;
+  }
+};
 
 wxIMPLEMENT_APP(CalculatorApp);
