3ddb7145c78e830de263336127b95670b2cc1cbc
Gleb Boiko <glebboyko@icloud.com>
2023-05-05 15:52:40 +0300

    Добавлен класс, изменена функция

--- frontend/visualization/visualization.hpp
+++ frontend/visualization/visualization.hpp
@@ -1,15 +1,36 @@
 #pragma once
 
 #include <memory>
+#include <string>
 
 #include "backend/calc_exec/calc_exec.hpp"
+#include "frontend/interface/definition/interface_def.hpp"
 
 namespace IV {
+class TextBlock {
+ public:
+  TextBlock();
+  explicit TextBlock(const ID::TextBlock&);
+  TextBlock(const TextBlock&) = delete;
+  TextBlock(TextBlock&&) = delete;
+
+  ~TextBlock();
+
+  TextBlock& operator=(const TextBlock&) = delete;
+  TextBlock& operator=(TextBlock&&);
+
+  void Update(const std::string&);
+
+ private:
+};
+
 // класс - визуализатор
 class Visualization {
  public:
   // конструкторы и деструкторы
-  Visualization(std::shared_ptr<CE::Calc>);
+  Visualization() = delete;
+  Visualization(std::shared_ptr<CE::Calc>,
+                const ID::VisualisationTemplate& visualisation_template);
   Visualization(const Visualization&);
   Visualization(Visualization&&);
 
@@ -26,6 +47,7 @@ class Visualization {
 };
 
 // функция, следящая за изменениями в калькуляторе (отдельный поток)
-void Updater(std::shared_ptr<CE::Calc> calc);
+void Updater(std::shared_ptr<CE::Calc> calc,
+             ID::VisualisationTemplate visualisation_template);
 
 }  // namespace IV
