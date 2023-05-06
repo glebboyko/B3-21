bb63716df69c9ccb36b82a16bc851771085daeda
Gleb Boiko <glebboyko@icloud.com>
2023-05-06 20:03:56 +0300

    Добавлена реализация методов

--- frontend/interface/frame/connection.cpp
+++ frontend/interface/frame/connection.cpp
@@ -1,3 +1,4 @@
+#include "backend/backup/backup.hpp"
 #include "frame.hpp"
 
 namespace IF {
@@ -7,6 +8,42 @@ void CalculatorFrame::ButtonClick(wxCommandEvent& event) {
     calc_->TurnOnOff();
   } else {
     calc_->PressButton(static_cast<CE::Button>(event.GetId()));
+  }
+}
+
+void CalculatorFrame::MakeBackUp(wxCommandEvent& event) {
+  wxFileDialog file_dialog(this, _("Make backup"), "", "",
+                           "CALC files (*.calc)|*.calc",
+                           wxFD_SAVE | wxFD_FILE_MUST_EXIST);
+  if (file_dialog.ShowModal() != wxID_CANCEL) {
+    BU::MakeBackup(*calc_, file_dialog.GetPath().ToStdString());
+  }
+}
+
+void CalculatorFrame::LoadFromBackUp(wxCommandEvent& event) {
+  wxFileDialog file_dialog(this, _("Load from backup"), "", "",
+                           "CALC files (*.calc)|*.calc",
+                           wxFD_OPEN | wxFD_FILE_MUST_EXIST);
+  if (file_dialog.ShowModal() != wxID_CANCEL) {
+    *calc_ = BU::RestoreFromBackup(file_dialog.GetPath().ToStdString());
+  }
+}
+
+void CalculatorFrame::SaveProgram(wxCommandEvent& event) {
+  wxFileDialog file_dialog(this, _("Load from backup"), "", "",
+                           "PROG files (*.prog)|*.prog",
+                           wxFD_SAVE | wxFD_FILE_MUST_EXIST);
+  if (file_dialog.ShowModal() != wxID_CANCEL) {
+    BU::SaveProgram(calc_->GetProgram(), file_dialog.GetPath().ToStdString());
+  }
+}
+
+void CalculatorFrame::LoadProgram(wxCommandEvent& event) {
+  wxFileDialog file_dialog(this, _("Load from backup"), "", "",
+                           "PROG files (*.prog)|*.prog",
+                           wxFD_OPEN | wxFD_FILE_MUST_EXIST);
+  if (file_dialog.ShowModal() != wxID_CANCEL) {
+    calc_->SetProgram(BU::LoadProgram(file_dialog.GetPath().ToStdString()));
   }
 }
 
