9931b09b9bb54679bc465361fa3de6a4e7776886
Gleb Boiko <glebboyko@icloud.com>
2023-05-16 17:12:28 +0300

    Добавлены wxThreads

--- main.cpp
+++ main.cpp
@@ -1,10 +1,29 @@
+#include <wx/wx.h>
+
 #include <memory>
-#include <thread>
 
 #include "backend/calc_exec/calc_exec.hpp"
 #include "frontend/interface/definition/interface_def.hpp"
 #include "frontend/interface/frame/frame.hpp"
 #include "frontend/visualization/visualization.hpp"
+
+class Updater : public wxThread {
+ public:
+  Updater(std::shared_ptr<CE::Calc> calc,
+          const ID::VisualisationTemplate& visualisation_template)
+      : wxThread(wxTHREAD_DETACHED),
+        calc_(calc),
+        vis_tem_(visualisation_template) {}
+
+  virtual void* Entry() {
+    IV::Updater(calc_, vis_tem_);
+    return nullptr;
+  }
+
+ private:
+  std::shared_ptr<CE::Calc> calc_;
+  ID::VisualisationTemplate vis_tem_;
+};
 
 class CalculatorApp : public wxApp {
  public:
@@ -14,8 +33,8 @@ class CalculatorApp : public wxApp {
     IF::CalculatorFrame* frame = new IF::CalculatorFrame("Calculator", calc);
     frame->Show(true);
 
-    std::thread updater = std::thread(IV::Updater, calc, frame->GetTemplate());
-    updater.detach();
+    Updater* updater = new Updater(calc, frame->GetTemplate());
+    updater->Run();
     return true;
   }
 };
