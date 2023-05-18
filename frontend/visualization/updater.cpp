43a5f76378fceca932b67b37c62431370b585ac3
Gleb Boiko <glebboyko@icloud.com>
2023-05-18 15:33:34 +0300

    Исправлена ошибка

--- frontend/visualization/updater.cpp
+++ frontend/visualization/updater.cpp
@@ -6,7 +6,8 @@ void Updater(std::shared_ptr<CE::Calc> calc, wxFrame* window) {
   while (true) {
     auto received_msg = queue_checker.Receive(1, MQ::Wait);
     if (received_msg.value() == CE::Error) {
-      wxMessageBox("Aaaaaaaaa", "Fatal Crash", wxOK | wxICON_ERROR);
+      wxMessageBox("An error occurred while using the calculator",
+                   "EXECUTING ERROR", wxOK | wxICON_ERROR);
     } else if (received_msg.value() == CE::UpdateData) {
       wxCommandEvent update_event(IF::CalculatorFrame::kEventUpdate, wxID_ANY);
       wxPostEvent(window, update_event);
@@ -16,4 +17,4 @@ void Updater(std::shared_ptr<CE::Calc> calc, wxFrame* window) {
   }
 }
 
-}  // namespace IV
+}  // namespace IU
